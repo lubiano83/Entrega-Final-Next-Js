0295e445e2a2fc202f351f38ac5683dd3db65691
lubiano83 <lubiano83@gmail.com>
2024-08-10 23:05:59 -0400

    ajustes varios

--- app/api 2/contacto/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(request){
-    const formData = await request.json();
-    console.log(formData);
-    return NextResponse.json({ message: 'Data recibida correctamente' });
-};

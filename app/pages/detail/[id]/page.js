acb334acacd3b46c3e846cfb5fbba53d9a797861
lubiano83 <lubiano83@gmail.com>
2024-08-14 15:12:41 -0400

    ajustes varios

--- app/pages/detail/[id]/page.js
+++ app/pages/detail/[id]/page.js
@@ -3,7 +3,7 @@ import ProductDetail from '../../../components/products/ProductDetail';
 
 export async function generateMetadata ({params, searchParams}, parent) {
   const { id } = params;
-  const items = await fetch(`http://localhost:3000/api/detail/${id}`, {cache: "no-store"}).then(res => res.json());
+  const items = await fetch(`http://localhost:3000/api/detail/${id}`);
 
   return {
     title: `${items.brand} - ${items.model}`,

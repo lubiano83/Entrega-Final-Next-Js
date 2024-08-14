7e72985232702b30122a179d8458cf14ab09533f
lubiano83 <lubiano83@gmail.com>
2024-08-13 21:55:38 -0400

    agregando generateMetadata en detail

--- app/pages/detail/[id]/page.js
+++ app/pages/detail/[id]/page.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ProductDetail from '../../../components/products/ProductDetail';
 
+export async function generateMetadata ({params, searchParams}, parent) {
+  const { id } = params;
+  const items = await fetch(`http://localhost:3000/api/detail/${id}`, {cache: "no-store"}).then(res => res.json());
+
+  return {
+    title: `${items.brand} - ${items.model}`,
+    description: items.description,
+  };
+};
+
 const Detail = ({ params }) => {
 
   const { id } = params;
 
   return (
     <div className="bg-white flex justify-evenly items-center h-full w-full">
-      <ProductDetail id={id} />
+      <ProductDetail id={id}/>
     </div>
   )
 }; export default Detail;

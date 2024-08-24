26a462ad371c2551fa830ab47cbc512d100e3653
José Pablo Lubiano <lubiano83@Mac-mini-de-Jose.local>
2024-08-24 19:44:51 -0400

    volviendo atras

--- app/pages/products/[category]/page.js
+++ app/pages/products/[category]/page.js
@@ -12,9 +12,13 @@ const Category = ({ params, searchParams }) => {
   return (
     <div className='w-full h-full flex'>
 <<<<<<< HEAD
+<<<<<<< HEAD
       <Suspense fallback={ <Title style='text-3xl flex justify-center items-center w-full h-full'>Cargando...</Title> }>
         <ProductsList category={category} limit={limit} page={page} sort={sort} />
       </Suspense>
+=======
+      <ProductsList category={category} limit={limit} page={page} sort={sort}/>
+>>>>>>> parent of a77e6e7 (agregando loading y suspense)
 =======
       <ProductsList category={category} limit={limit} page={page} sort={sort}/>
 >>>>>>> parent of a77e6e7 (agregando loading y suspense)

3e134b98f135f746a1ffed5156c1e6a2a97466ef
José Pablo Lubiano <lubiano83@Mac-mini-de-Jose.local>
2024-08-24 20:33:08 -0400

    ajustes varios

--- app/pages/products/[category]/page.js
+++ app/pages/products/[category]/page.js
@@ -11,17 +11,16 @@ const Category = ({ params, searchParams }) => {
 
   return (
     <div className='w-full h-full flex'>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <Suspense fallback={ <Title style='text-3xl flex justify-center items-center w-full h-full'>Cargando...</Title> }>
         <ProductsList category={category} limit={limit} page={page} sort={sort} />
       </Suspense>
-=======
+
       <ProductsList category={category} limit={limit} page={page} sort={sort}/>
->>>>>>> parent of a77e6e7 (agregando loading y suspense)
-=======
+
+
       <ProductsList category={category} limit={limit} page={page} sort={sort}/>
->>>>>>> parent of a77e6e7 (agregando loading y suspense)
+
     </div>
   )
 }; export default Category;

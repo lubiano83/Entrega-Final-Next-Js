1de779528135e5bfefe5171a6456cdb618287f0c
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-08-25 16:35:45 -0400

    ajustes varios

--- app/pages/detail/[id]/page.js
+++ app/pages/detail/[id]/page.js
@@ -3,10 +3,17 @@ import ProductDetail from '../../../components/products/ProductDetail';
 
 export async function generateMetadata ({params}) {
   const { id } = params;
-  const items = await fetch(`http://localhost:3000/api/detail/${id}`);
-  
   return {
-    title: `AutoShop - ${id}`,
+    title: `AutoShop: ${id}`,
+    description: "Venta en respuestos y accesorios de vehiculos en linea",
+    keywords: ["autoshop", "repuestos", "accesorios", "autos", "baterias", "neumaticos", "online", ],
+    openGraph: { // extension para el uso de redes sociales.
+      title: `AutoShop: ${id}`,
+      description: "AutoShop ahora con su tienda online",
+      type: "website",
+      publishedTime: "2023-01-01T00:00:00Z",
+      authors: ["lubiano83"]
+    },
   };
 };
 

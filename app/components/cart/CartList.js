5368caf5dbdb9d5f640a98ded5aed76c705f5854
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-08-26 17:24:29 -0400

    ajustes varios

--- app/components/cart/CartList.js
+++ app/components/cart/CartList.js
@@ -7,18 +7,20 @@ import { useCart } from '@/app/hooks/useCart';
 
 const CartList = () => {
 
-  const { cart, getTotalPrice, getTotalQuantity, removeItem } = useCart();
+  const { cart, getTotalPrice, getTotalQuantity, removeItem, totalQuantity } = useCart();
 
   return (
     <div className='bg-white h-full w-full flex flex-col justify-between items-center text-center px-8'>
       <div className='p-8'>
         <Title style="text-3xl">Productos en Carrito:</Title>
       </div>
-      <div className='w-full h-full flex flex-col justify-start gap-3'>
-        {
-          cart.map(prod => (
-            <CartCard key={prod.item.id} item={prod.item} counter={prod.counter} removeItem={removeItem} />
-          ))
+      <div className='w-full h-full flex flex-col justify-center gap-3 text-gray-700'>
+        {totalQuantity === 0 ? <p className="text-xl">No hay productos en el carrito...</p> : 
+          (
+            cart.map(prod => (
+              <CartCard key={prod.item.id} item={prod.item} counter={prod.counter} removeItem={removeItem} />
+            ))
+          )
         }
       </div>
       <div>

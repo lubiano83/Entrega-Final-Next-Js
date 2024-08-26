9c14b63f6f9b35df45bfd4ba4d14527660c74498
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2024-08-25 23:11:31 -0400

    agregando archivo .env

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYtHOGAh73OBje0zbhcRICYyMow6Ci5xo",
-  authDomain: "next-js-459e0.firebaseapp.com",
-  projectId: "next-js-459e0",
-  storageBucket: "next-js-459e0.appspot.com",
-  messagingSenderId: "940637024695",
-  appId: "1:940637024695:web:669d92a0bf44da5b263832"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);

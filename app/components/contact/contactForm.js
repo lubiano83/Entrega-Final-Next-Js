7fb1ff93f9e74d3ed0c7f675dc490089541d6233
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-08-26 17:11:03 -0400

    agregando sweet alert

--- app/components/contact/contactForm.js
+++ app/components/contact/contactForm.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from 'react'
 import Button from '../Button';
+import Swal from 'sweetalert2';
 
 const ContactForm = () => {
 
@@ -17,8 +18,18 @@ const ContactForm = () => {
   };
 
   const handleSubmit = async (event) => {
-    // event.preventDefault();
+    event.preventDefault();
     await fetch(`http://localhost:3000/api/contact`, { method: 'POST', body: JSON.stringify(values)});
+  };
+
+  const sendMessage = () => {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Mensaje enviado con exito!!",
+      showConfirmButton: false,
+      timer: 1500
+    });
   };
 
   return (
@@ -27,7 +38,7 @@ const ContactForm = () => {
       <textarea required placeholder='Dejanos tu Mensaje..' name="email" onChange={handleChange} className='w-1/2 min-w-72 h-72 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 border-gray-700 text-lg' />
       <div className='flex justify-center items-center gap-2'>
         <Button type="reset">Limpiar</Button>
-        <Button type="submit">Enviar</Button>
+        <Button type="submit" handleClick={sendMessage}>Enviar</Button>
       </div>
     </form>
   )

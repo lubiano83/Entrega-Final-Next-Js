e587815c9ebec3ffbe9a8ad994402102fcc30d3b
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2024-09-02 05:40:06 -0400

    ajustes varios

--- app/api/contacts/route.js
+++ app/api/contacts/route.js
@@ -4,32 +4,26 @@ import { doc, setDoc, getDoc, getDocs, collection } from 'firebase/firestore';
 
 export async function GET() {
   try {
-    // Referencia a la colección de contactos
     const collectionRef = collection(db, 'contacts');
     const querySnapshot = await getDocs(collectionRef);
 
-    // Obtener la fecha de hace 7 días
     const sevenDaysAgo = new Date();
     sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 
-    // Mapeo y ordenamiento de datos
     const contacts = querySnapshot.docs.map(doc => {
       const data = doc.data();
       const messages = data.messages || [];
 
-      // Filtra mensajes de los últimos 7 días
       const recentMessages = messages.filter(message => new Date(message.timestamp) >= sevenDaysAgo);
 
-      // Ordena los mensajes en orden descendente por timestamp
       recentMessages.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
       
       return {
         email: doc.id,
         messages: recentMessages
       };
-    }).filter(contact => contact.messages.length > 0); // Filtra contactos sin mensajes recientes
-
-    // Ordena los contactos en orden descendente por el timestamp del mensaje más reciente
+    }).filter(contact => contact.messages.length > 0);
+    
     contacts.sort((a, b) => {
       const lastMessageA = a.messages[0] ? new Date(a.messages[0].timestamp) : new Date(0);
       const lastMessageB = b.messages[0] ? new Date(b.messages[0].timestamp) : new Date(0);

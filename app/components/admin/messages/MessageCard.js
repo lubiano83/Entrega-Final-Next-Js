4973221fd95f31bf3e0412473b9403f465249055
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2024-09-02 02:11:16 -0400

    ajustes varios

--- app/components/admin/messages/MessageCard.js
+++ app/components/admin/messages/MessageCard.js
@@ -9,14 +9,14 @@ const MessageCard = ({ email, messages }) => {
     return <div className='text-gray-700 flex justify-center items-center'>No hay datos disponibles</div>;
   }
 
-  const recentMessages = messages.slice(0, 3);
+  const recentMessages = messages.slice(0, 1);
 
   return (
-    <div className={`p-4 border-2 rounded-2xl shadow-md w-full bg-gray-700 text-white ${isDarkMode ? "border-orange-600" : "border-blue-600"}`}>
+    <div className={`p-2 border-2 rounded-2xl shadow-md w-full bg-gray-700 text-white ${isDarkMode ? "border-orange-600" : "border-blue-600"}`}>
       <h2 className={`text-lg font-semibold ${isDarkMode ? "text-orange-600" : "text-blue-600"}`}>{email}</h2>
       <ul>
         {recentMessages.map((message, index) => (
-          <li key={index} className='py-2 border-b border-gray-200 flex flex-col'>
+          <li key={index} className='flex flex-col'>
             <p className='text-md'>{message.text}</p>
             <small className='text-sm text-gray-400'>{new Date(message.timestamp).toLocaleString()}</small>
           </li>

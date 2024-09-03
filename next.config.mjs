87bab961609b7d6de75130b39f67c379ca92f576
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-09-03 08:51:37 -0400

    ajustes varios

--- next.config.mjs
+++ next.config.mjs
@@ -16,17 +16,6 @@ const nextConfig = {
       }
     ],
   },
-
-  // Configuración de las variables de entorno
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Si también necesitas esta variable
-  },
 };
 
 export default nextConfig;

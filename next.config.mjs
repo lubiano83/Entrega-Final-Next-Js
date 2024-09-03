6dd004b1607a38a5e4fac032bb3d4470d8b1a968
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-09-03 08:42:26 -0400

    ajustes varios

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'firebasestorage.googleapis.com',
-          pathname: '/v0/b/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'raw.githubusercontent.com',
-          port: "",
-          pathname: '/**',
-        }
-      ],
-    },
-  }; export default nextConfig;
+  // Configuración de las imágenes remotas permitidas
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: "",
+        pathname: '/**',
+      }
+    ],
+  },
+
+  // Configuración de las variables de entorno
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Si también necesitas esta variable
+  },
+};
+
+export default nextConfig;

40980bdb6b707bc7be031ac2a533c9e33ddcd38b
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-09-06 17:49:57 -0400

    ajustes varios

--- app/components/detail/DetailCard.js
+++ app/components/detail/DetailCard.js
@@ -10,29 +10,35 @@ const DetailCard = ({id, imageUrl, category, brand, model, quantity, price, desc
     const { setPrice } = usePrice();
     const { capitalize } = useCapitalize();
 
-  return (
-    <>
-        <div className='flex items-center flex-wrap justify-center'>
-            <div className='bg-white'>
-                <Image src={imageUrl} alt={model} height={288} width={288} className='bg-white active:scale-150 cursor-pointer' />
-            </div>
-            <div className='p-4 text-gray-700 h-72 flex flex-col items-start justify-center gap-2 text-xl'>
-                <p><strong>Id:</strong> {id}</p>
-                <p><strong>Categoria:</strong> {capitalize(category)}</p>
-                <p><strong>Marca:</strong> {capitalize(brand)}</p>
-                <p><strong>Modelo:</strong> {model}</p>
-                <p><strong>Descripcion:</strong> {description}</p>
-                <p><strong>Cantidad:</strong> {quantity}</p>
-                <p><strong>Estado:</strong><span className={`${status === true && quantity > 0 ? "text-gray-700" : "text-red-500"}`}> {status === true && quantity > 0 ? "Disponible" : "No Disponible"}</span></p>
-                <p><strong>Precio:</strong> ${price ? setPrice(price) : ""}</p>
-            </div>
-        </div>
-        <div className='px-7 text-gray-700 w-1/2 text-m'>
-        <p><strong>Detalle:</strong> {detail}</p>
-        </div>
-        <div>
-            { status === true && quantity > 0 ? <Counter item={item} /> : "" }
-        </div>
-    </>
-  )
+    try {
+        return (
+          <>
+              <div className='flex items-center flex-wrap justify-center'>
+                  <div className='bg-white'>
+                      <Image src={imageUrl} alt={model} height={288} width={288} className='bg-white active:scale-150 cursor-pointer' />
+                  </div>
+                  <div className='p-4 text-gray-700 h-72 flex flex-col items-start justify-center gap-2 text-xl'>
+                      <p><strong>Id:</strong> {id}</p>
+                      <p><strong>Categoria:</strong> {capitalize(category)}</p>
+                      <p><strong>Marca:</strong> {capitalize(brand)}</p>
+                      <p><strong>Modelo:</strong> {model}</p>
+                      <p><strong>Descripcion:</strong> {description}</p>
+                      <p><strong>Cantidad:</strong> {quantity}</p>
+                      <p><strong>Estado:</strong><span className={`${status === true && quantity > 0 ? "text-gray-700" : "text-red-500"}`}> {status === true && quantity > 0 ? "Disponible" : "No Disponible"}</span></p>
+                      <p><strong>Precio:</strong> ${price ? setPrice(price) : ""}</p>
+                  </div>
+              </div>
+              <div className='px-7 text-gray-700 w-1/2 text-m'>
+              <p><strong>Detalle:</strong> {detail}</p>
+              </div>
+              <div>
+                  { status === true && quantity > 0 ? <Counter item={item} /> : "" }
+              </div>
+          </>
+        )
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    }
+
 }; export default DetailCard;
--- app/components/products/ProductCard.js
+++ app/components/products/ProductCard.js
@@ -13,24 +13,30 @@ const ProductCard = ({ id, imageUrl, category, brand, model, quantity, price, de
   const { isDarkMode } = useDarkMode();
   const { capitalize } = useCapitalize()
 
-  return (
-    <article className={`border-2 ${isDarkMode ? "border-orange-600" : "border-blue-600"} w-72 rounded-3xl overflow-hidden bg-gray-700 shadow-md shadow-gray-700`}>
-      <div className='bg-white'>
-        <Image priority={false} src={imageUrl} alt={`autoshop, ${category}, ${model}, ${brand}, ${description}`} height={288} width={288} />
-      </div>
-      <div className='p-4 text-white'>
-        <p>Categoria: {category ? capitalize(category) : ""}</p>
-        <p>Marca: {brand ? capitalize(brand) : ""}</p>
-        <p>Modelo: {model}</p>
-        <p>Descripcion: {description}</p>
-        <p>Cantidad: {quantity}</p>
-        <p>Precio: ${price ? setPrice(price) : ""}</p>
-      </div>
-      <div className='text-center pb-4'>
-        <Link href={`/pages/product/${id}`}>
-          <Button>Detalle</Button>
-        </Link>
-      </div>
-    </article>
-  )
+  try {
+    return (
+      <article className={`border-2 ${isDarkMode ? "border-orange-600" : "border-blue-600"} w-72 rounded-3xl overflow-hidden bg-gray-700 shadow-md shadow-gray-700`}>
+        <div className='bg-white'>
+          <Image priority={false} src={imageUrl} alt={`autoshop, ${category}, ${model}, ${brand}, ${description}`} height={288} width={288} />
+        </div>
+        <div className='p-4 text-white'>
+          <p>Categoria: {category ? capitalize(category) : ""}</p>
+          <p>Marca: {brand ? capitalize(brand) : ""}</p>
+          <p>Modelo: {model}</p>
+          <p>Descripcion: {description}</p>
+          <p>Cantidad: {quantity}</p>
+          <p>Precio: ${price ? setPrice(price) : ""}</p>
+        </div>
+        <div className='text-center pb-4'>
+          <Link href={`/pages/product/${id}`}>
+            <Button>Detalle</Button>
+          </Link>
+        </div>
+      </article>
+    )
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+
 }; export default ProductCard;

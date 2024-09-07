1049bfe246f39999a6031b29229afb17c84d2aa6
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-09-07 10:33:49 -0400

    ajustes varios

--- app/components/admin/create/CreateForm.js
+++ app/components/admin/create/CreateForm.js
@@ -69,48 +69,60 @@ const CreateForm = () => {
   };
 
   const handleReset = () => {
-    setValues(initialValues);
-    setImageFile(null);
-  };
-
-  const sendMessage = (icon, message) => {
-    Swal.fire({
-      position: "center",
-      icon: icon,
-      title: message,
-      showConfirmButton: false,
-      timer: 1500
-    });
-    if (icon === "success") {
-      router.refresh();
-      setTimeout(handleReset, 1500);
+    try {
+      setValues(initialValues);
+      setImageFile(null);
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
-  return (
-    <div className={`w-1/2 min-w-72 p-8 gap-4 rounded-3xl flex flex-wrap flex-col justify-center items-center bg-gray-700 bg-opacity-25`}>
-      <Title style="text-3xl">Ingreso de Productos:</Title>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-4 w-full'>
-        <input type="file" required name="image" onChange={handleChange} className='min-w-60 h-10 text-gray-700 text-lg text-center' />
-        <input type="text" required placeholder='Ingresa una Categoria..' name="category" value={values.category} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="text" required placeholder='Ingresa una Marca..' name="brand" value={values.brand} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="text" required placeholder='Ingresa un Modelo..' name="model" value={values.model} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="text" required placeholder='Ingresa una Descripción..' name="description" value={values.description} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="text" required placeholder='Ingresa un Filtro..' name="filter" value={values.filter} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="number" required placeholder='Ingresa un Cantidad..' name="quantity" value={values.quantity} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="number" required placeholder='Ingresa un Precio..' name="price" value={values.price} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <input type="text" required placeholder='Ingresa un Detalle..' name="detail" value={values.detail} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-          <label className="text-lg text-center flex justify-center items-center text-gray-700 gap-2">
-            <input type="checkbox" name="status" checked={values.status} onChange={handleChange} className="form-checkbox h-5 w-5 text-blue-600" />
-              { values.status === true ? "Producto disponible" : "Producto no disponible" }
-            </label>
-        </div>
-        <div className='flex justify-center gap-2'>
-          <Button color="danger" size="md" style="w-24" type="reset" onClick={handleReset}>Limpiar</Button>
-          <Button color="primary" size="md" style="w-24" type="submit">Ingresar</Button>
-        </div>
-      </form>
-    </div>
-  );
+  const sendMessage = (icon, message) => {
+    try {
+      Swal.fire({
+        position: "center",
+        icon: icon,
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+      });
+      if (icon === "success") {
+        router.refresh();
+        setTimeout(handleReset, 1500);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  try {
+    return (
+      <div className={`w-1/2 min-w-72 p-8 gap-4 rounded-3xl flex flex-wrap flex-col justify-center items-center bg-gray-700 bg-opacity-25`}>
+        <Title style="text-3xl">Ingreso de Productos:</Title>
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-4 w-full'>
+          <input type="file" required name="image" onChange={handleChange} className='min-w-60 h-10 text-gray-700 text-lg text-center' />
+          <input type="text" required placeholder='Ingresa una Categoria..' name="category" value={values.category} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="text" required placeholder='Ingresa una Marca..' name="brand" value={values.brand} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="text" required placeholder='Ingresa un Modelo..' name="model" value={values.model} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="text" required placeholder='Ingresa una Descripción..' name="description" value={values.description} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="text" required placeholder='Ingresa un Filtro..' name="filter" value={values.filter} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="number" required placeholder='Ingresa un Cantidad..' name="quantity" value={values.quantity} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="number" required placeholder='Ingresa un Precio..' name="price" value={values.price} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <input type="text" required placeholder='Ingresa un Detalle..' name="detail" value={values.detail} onChange={handleChange} className={`w-full min-w-60 h-10 rounded-xl px-2 shadow-gray-700 shadow-sm text-gray-700 border-2 text-lg ${isDarkMode ? "border-orange-600" : "border-blue-600"}`} />
+          <div className='flex flex-wrap items-center justify-center gap-4'>
+            <label className="text-lg text-center flex justify-center items-center text-gray-700 gap-2">
+              <input type="checkbox" name="status" checked={values.status} onChange={handleChange} className="form-checkbox h-5 w-5 text-blue-600" />
+                { values.status === true ? "Producto disponible" : "Producto no disponible" }
+              </label>
+          </div>
+          <div className='flex justify-center gap-2'>
+            <Button color="danger" size="md" style="w-24" type="reset" onClick={handleReset}>Limpiar</Button>
+            <Button color="primary" size="md" style="w-24" type="submit">Ingresar</Button>
+          </div>
+        </form>
+      </div>
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
 }; export default CreateForm;

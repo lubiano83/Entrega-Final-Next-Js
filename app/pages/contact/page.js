6aa22fc99a7351c5d813efd740754f1764bd18b1
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2024-09-18 11:14:05 -0300

    ajustes varios

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -4,7 +4,7 @@ import ContactForm from '@/app/components/contact/contactForm';
 const Contact = () => {
 
   return (
-    <div className='bg-white flex flex-col justify-center items-center text-gray-700 h-full w-full gap-4 m-8'>
+    <div className='bg-white flex flex-col justify-center items-center text-gray-700 h-full w-full gap-4 p-8'>
       <ContactForm />
     </div>
   )
